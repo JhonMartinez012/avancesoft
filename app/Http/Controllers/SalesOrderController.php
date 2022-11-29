36786c9738b8b3eaf1b2f93d446e2852128f548a
Jhon A. Martinez R <95629824+JhonMartinez012@users.noreply.github.com>
2022-11-29 13:32:29 -0500

    Punto 2 del 2 terminado y testeado

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -10,8 +10,23 @@ class SalesOrderController extends Controller
     //
     public function salesOrderByCustomer($custId)
     {
-        $salesOrders = SalesOrder::with('customer', 'employee', 'shipper')
-                   ->where('custId', $custId)->paginate(10);
-        return response()->json($salesOrders);
+
+        try {
+
+            $salesOrdersExist = SalesOrder::where('custId', $custId)->exists();
+
+            if (!$salesOrdersExist) {
+                return response()->json([
+                    'message' => 'No hay ordenes de compra para este cliente',
+                ], 404);
+            }
+
+            $salesOrders = SalesOrder::with('customer', 'employee', 'shipper')
+                ->where('custId', $custId)->paginate(10);
+                
+            return response()->json($salesOrders, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'products'], function () {
     Route::get('/', [ProductController::class, 'index']);
+});
+
+Route::group(['prefix' => 'sales-orders'], function () {
+    Route::get('/{custId}', [SalesOrderController::class, 'salesOrderByCustomer']);
 });
