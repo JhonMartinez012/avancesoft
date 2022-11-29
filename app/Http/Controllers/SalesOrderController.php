0d627d6770eabde6337f0d52e694d3ad9e45854d
Jhon A. Martinez R <95629824+JhonMartinez012@users.noreply.github.com>
2022-11-29 12:05:06 -0500

    punto 2 del 2 funcion por probar

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SalesOrder;
 use Illuminate\Http\Request;
 
 class SalesOrderController extends Controller
 {
     //
+    public function salesOrderByCustomer($custId)
+    {
+        $salesOrders = SalesOrder::with('customer', 'employee', 'shipper')
+                   ->where('custId', $custId)->paginate(10);
+        return response()->json($salesOrders);
+    }
 }
